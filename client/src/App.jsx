fb7306e6ae3c05c8a557b602f959dd69cec6a2ed
Tan <ngoctana1k2014@gmail.com>
2023-01-11 23:15:28 +0700

    login UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import PageNotFound from './components/PageNotFound';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Username from './components/Username';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/password',
+    element: <Password />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+  {
+    path: '/recovery',
+    element: <Recovery />,
+  },
+  {
+    path: '/reset',
+    element: <Reset />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
+  },
+]);
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
 };
 
 export default App;
